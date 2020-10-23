94228264a762c503a3550f3fbc1f983a8f90bc58
Michel Dänzer <mdaenzer@redhat.com>
2020-10-23 19:03:49 +0200

    Drop config directory
    Not used anymore on this branch.
    
    (cherry picked from commit 23296633bb221242aac631a542762cca240abf4f)
    (cherry picked from commit d07af46a67bc8e68fa050c605bb556bc25886f16)
    
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1483>

--- dix/main.c
+++ dix/main.c
@@ -104,7 +104,6 @@ Equipment Corporation.
 #include <X11/fonts/libxfont2.h>
 #include "opaque.h"
 #include "servermd.h"
-#include "hotplug.h"
 #include "dixfont.h"
 #include "extnsionst.h"
 #include "privates.h"
