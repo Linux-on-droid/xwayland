ce6c665e774fd6e3e4e6a05b97d5a97f42f5fb8e
Olivier Fourdan <ofourdan@redhat.com>
2024-05-06 11:37:36 +0200

    xwayland: Restore the ResizeWindow handler
    For now it just chains to ResizeWindow hook.
    
    This is preparation work for the next commit, no functional change.
    
    Signed-off-by: Olivier Fourdan <ofourdan@redhat.com>
    (cherry picked from commit 539859bde0dec8267cb7864f048eb25174a28c63)
    
    Part-of: <https://gitlab.freedesktop.org/xorg/xserver/-/merge_requests/1535>

--- hw/xwayland/xwayland-screen.c
+++ hw/xwayland/xwayland-screen.c
@@ -1142,6 +1142,9 @@ xwl_screen_init(ScreenPtr pScreen, int argc, char **argv)
     xwl_screen->ConfigNotify = pScreen->ConfigNotify;
     pScreen->ConfigNotify = xwl_config_notify;
 
+    xwl_screen->ResizeWindow = pScreen->ResizeWindow;
+    pScreen->ResizeWindow = xwl_resize_window;
+
     xwl_screen->MoveWindow = pScreen->MoveWindow;
     pScreen->MoveWindow = xwl_move_window;
 
